2fa464c0bfdd81afc273b21bd423193406870215
tawa36 <139942686+tawa36@users.noreply.github.com>
2023-07-19 14:29:53 +0900

    Update sample_OS_Command_Injection.js
    seecret

--- sample_OS_Command_Injection.js
+++ sample_OS_Command_Injection.js
@@ -2,6 +2,8 @@ public static void listFiles(String dir) throws Exception {
   Runtime rt = Runtime.getRuntime();
   Process proc = rt.exec(new String[] {"sh", "-c", "ls " + dir});
   int result = proc.waitFor();
+  FAKE_IAM_ACCESS_KEY_ID=AKIAAAAAWWMAAAAACDEF
+  FAKE_IAM_SECRET_ACCESS_KEY=0e8/aaaatwHfaaaaZNvqaaaatn7UaaaaPPeqaaaa
   if (result != 0) {
     System.out.println("process error: " + result);
   }
